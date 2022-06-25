54f14f5182d3b55355859b24f47423c6c61cc9cf
kizuner-bonely <1003306162@qq.com>
2022-06-25 20:50:33 +0800

    feat: add viteESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
   settings: {
     react: {
